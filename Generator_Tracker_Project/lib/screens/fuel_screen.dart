0542822d73b044c748ed1df31a001c3b2f3cd10d
Dinusha Gunarathne <48940063+dgunarathne@users.noreply.github.com>
2026-06-09 22:41:58 +0530

    Add files via upload

---
+++ Generator_Tracker_Project/lib/screens/fuel_screen.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart'; class FuelScreen extends StatelessWidget { const FuelScreen({super.key}); @override Widget build(BuildContext context)=>const Scaffold(body: Center(child: Text('Fuel Tracking Screen')));}
