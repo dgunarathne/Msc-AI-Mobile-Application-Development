3713d16fbb0cdfa425417ea465a2778a087a84c6
Dinusha Gunarathne <48940063+dgunarathne@users.noreply.github.com>
2026-06-09 22:22:12 +0530

    Update widget_test.dart

--- Generator_Tracker_Project/test/widget_test.dart
+++ Generator_Tracker_Project/test/widget_test.dart
@@ -1,10 +1,25 @@
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
 import 'package:generator_tracker/main.dart';
 
 void main() {
-  testWidgets('App loads', (tester) async {
-    await tester.pumpWidget(const GeneratorTrackerApp());
-    expect(find.text('Generator Tracker'), findsOneWidget);
-  });
+
+  testWidgets(
+    'App Loads',
+    (WidgetTester tester) async {
+
+      await tester.pumpWidget(
+        const MyApp(),
+      );
+
+      expect(
+        find.text(
+          'Generator Tracker',
+        ),
+        findsOneWidget,
+      );
+    },
+  );
 }
