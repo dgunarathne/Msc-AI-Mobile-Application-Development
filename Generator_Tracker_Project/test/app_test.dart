67dcf6002b0a9ea881a6a7e02648fccf4c9567d0
Dinusha Gunarathne <48940063+dgunarathne@users.noreply.github.com>
2026-06-09 22:22:42 +0530

    Create app_test.dart

---
+++ Generator_Tracker_Project/test/app_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  testWidgets(
+    "Application Starts",
+    (tester) async {
+
+      expect(
+        true,
+        true,
+      );
+    },
+  );
+}
